6ab1e618bc56a0699f032a7575cd88048e6094f1
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2024-08-07 03:24:24 +0600

    Remove test script from Echo and UniqueIdGen

--- Echo/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eux
-dotnet publish -c Release
-~/Downloads/maelstrom/maelstrom test -w echo --bin ./bin/Release/net7.0/publish/Echo --node-count 10 --time-limit 1
-~/Downloads/maelstrom/maelstrom serve
--- UniqueIdGen/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eux
-clear
-dotnet publish -c Release
-~/Downloads/maelstrom/maelstrom test -w unique-ids --bin ./bin/Release/net7.0/publish/UniqueIdGen --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition --log-stderr true
