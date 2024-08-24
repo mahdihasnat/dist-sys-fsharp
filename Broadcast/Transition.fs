a273cda6c7eae7a3c36288cea4bc223e61e98ff7
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2024-08-24 18:34:00 +0600

    Move graph generator to utilities

--- Broadcast/Transition.fs
+++ Broadcast/Transition.fs
@@ -25,26 +25,6 @@ let removeTimeoutPendingAck (node: Node) : Node =
             TimedOutMessages = node.TimedOutMessages |> Map.toList |> List.append (timedOutMessages |> Map.toList) |> Map.ofList
     }
 
-let generateGraph (nodes: NonEmptySet<NodeId>) : Map<NodeId, Set<NodeId>> =
-    let nodes = nodes |> NonEmptySet.toSeq
-    let n = nodes |> Seq.length
-    let k = Math.Sqrt (float n) |> int
-    let blocks = nodes |> Seq.chunkBySize k
-    let edgesWithinBlock : seq<NodeId * NodeId> =
-        blocks
-        |> Seq.collect (fun nodes -> Seq.allPairs nodes nodes)
-    let edgesBetweenBlocks : seq<NodeId * NodeId> =
-        blocks
-        |> Seq.collect (Seq.indexed)
-        |> Seq.groupBy fst
-        |> Seq.map snd
-        |> Seq.map (Seq.map snd)
-        |> Seq.collect (fun nodes -> Seq.allPairs nodes nodes)
-    Seq.append edgesWithinBlock edgesBetweenBlocks
-    |> Seq.groupBy fst
-    |> Seq.map (fun (src, dests) -> src, dests |> Seq.map snd |> Set.ofSeq |> Set.remove src)
-    |> Map.ofSeq
-
 let transition (node: Node) (action: Choice<Message<InputMessageBody>,unit>) : Node * List<Message<OutputMessageBody>> =
     let totalMessages= node.PendingAck.Count
     let node = removeTimeoutPendingAck node
