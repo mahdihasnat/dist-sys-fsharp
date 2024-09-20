9bb15ba695843561fc8a6f7df20e40779179a9fc
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2024-09-20 21:45:34 +0600

    Fix test.sh

--- GrowOnlyCounterFtSeqKV/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eux
-clear
-dotnet publish -c Release
-~/Downloads/maelstrom/maelstrom test -w g-counter --bin ./bin/Release/net7.0/publish/GrowOnlyCounterFtSeqKV --node-count 3 --rate 100 --time-limit 20 --nemesis partition --log-stderr
--- Kafka/test.sh
+++ Kafka/test.sh
@@ -3,5 +3,5 @@
 set -eux
 clear
 dotnet publish -c Release
-~/Downloads/maelstrom/maelstrom test -w kafka --bin ./bin/Release/net7.0/publish/Kafka --node-count 1 --concurrency 2n --time-limit 20 --rate 1000 --log-stderr
-# ~/Downloads/maelstrom/maelstrom test -w kafka --bin ./bin/Release/net7.0/publish/Kafka --node-count 2 --concurrency 2n --time-limit 20 --rate 1000 --log-stderr
+./../maelstrom/maelstrom test -w kafka --bin ./bin/Release/net7.0/publish/Kafka --node-count 1 --concurrency 2n --time-limit 20 --rate 1000 --log-stderr
+# ./../maelstrom/maelstrom test -w kafka --bin ./bin/Release/net7.0/publish/Kafka --node-count 2 --concurrency 2n --time-limit 20 --rate 1000 --log-stderr
