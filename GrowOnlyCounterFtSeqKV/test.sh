98b27b522902537613859fe8d90e8c060e76f7d2
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2024-10-05 00:42:03 +0600

    g-counter-seq-kv: simple test.sh

--- GrowOnlyCounterFtSeqKV/test.sh
+++ GrowOnlyCounterFtSeqKV/test.sh
@@ -2,10 +2,6 @@
 
 set -eux
 clear
-echo "DOTNET_ROOT=$DOTNET_ROOT"
+export DOTNET_ROOT=/usr/lib/dotnet
 dotnet publish -c Release
-for i in {1..100};
-do
-    touch $i
-    ./../maelstrom/maelstrom test -w g-counter --bin ./bin/Release/net7.0/publish/GrowOnlyCounterFtSeqKV --node-count 3 --rate 100 --time-limit 20 --nemesis partition
-done
+./../maelstrom/maelstrom test -w g-counter --bin ./bin/Release/net7.0/publish/GrowOnlyCounterFtSeqKV --node-count 3 --rate 100 --time-limit 20 --nemesis partition
