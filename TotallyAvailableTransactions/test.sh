3f1d589e4f9a6f05b24c0aaad3107551ebf9f631
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2024-10-05 12:28:09 +0600

    transactions: add 6b in test.sh

--- TotallyAvailableTransactions/test.sh
+++ TotallyAvailableTransactions/test.sh
@@ -4,4 +4,10 @@ set -eux
 clear
 export DOTNET_ROOT=/usr/lib/dotnet
 dotnet publish -c Release
+# 6a
 ./../maelstrom/maelstrom test -w txn-rw-register --bin ./bin/Release/net7.0/publish/TotallyAvailableTransactions --node-count 1 --time-limit 20 --rate 1000 --concurrency 2n --consistency-models read-uncommitted --availability total
+
+# 6b
+# ./../maelstrom/maelstrom test -w txn-rw-register --bin ./bin/Release/net7.0/publish/TotallyAvailableTransactions --node-count 2 --concurrency 2n --time-limit 20 --rate 1000 --consistency-models read-uncommitted
+# ./../maelstrom/maelstrom test -w txn-rw-register --bin ./bin/Release/net7.0/publish/TotallyAvailableTransactions --node-count 2 --concurrency 2n --time-limit 20 --rate 1000 --consistency-models read-uncommitted --availability total --nemesis partition
+
